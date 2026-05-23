50f4808f7d37bff1bddb622f1e75568895746361
Ledz3p <140008572+Ledz3p@users.noreply.github.com>
2026-05-23 17:25:31 -0500

    Initial Code upload to run web app

---
+++ Models/HistorySnapshot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace YarnInventoryWeb.Models
+{
+    public class HistorySnapshot
+    {
+        public List<YarnItem> Items { get; set; } = new();
+
+        public HistorySnapshot() { }
+
+        public HistorySnapshot(List<YarnItem> items)
+        {
+            // Clone every single item to prevent reference mutations in the stack
+            Items = items.Select(i => i.Clone()).ToList();
+        }
+    }
+}
---
+++ Models/YarnItem.cs
@@ -0,0 +1,26 @@
+﻿namespace YarnInventoryWeb.Models
+{
+    public class YarnItem
+    {
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+        public string Title { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty;
+        public int InStock { get; set; }
+        public int InBag { get; set; }
+        public bool IsDiscontinued { get; set; }
+
+        // Creates a deep copy of the item so history states don't share references
+        public YarnItem Clone()
+        {
+            return new YarnItem
+            {
+                Id = this.Id,
+                Title = this.Title,
+                Brand = this.Brand,
+                InStock = this.InStock,
+                InBag = this.InBag,
+                IsDiscontinued = this.IsDiscontinued
+            };
+        }
+    }
+}
